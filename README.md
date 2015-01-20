de9eeeb1325e0cbac3513535d5c6b9d429f8ddb9
guihua <guihua.pgh@alibaba-inc.com>
2015-01-20 11:20:03 +0800

    commit changes

--- README.md
+++ README.md
@@ -1,6 +1,51 @@
-gulp.js
-=======
+Gulp前端项目构建
+==============
 
-gulp.js
+##Gulp介绍
+Gulp基于Node.js的前端构建工具。它通过Gulp的插件可以实现前端代码的编译（sass、less）、压缩、测试，图片的压缩，浏览器自动刷新等。
 
-gulp.js config package.json and gulpfile.js
+###Gulp安装
+首先，打开终端，全局安装Gulp：
+
+	npm install gulp -g
+
+安装完成后，查看安装的Gulp版本：
+	
+	gulp -v
+
+显示：
+
+	CLI version 3.8.10
+	Local version 3.8.10
+
+到此，Gulp安装成功。
+
+###项目环境部署
+1.进入项目所在目录
+
+	cd projectName
+
+2.安装开发包和项目依赖文件
+
+	npm install
+
+NodeJS会根据package.json文件中的依赖关系自动安装相关插件，并部署到项目目录。
+
+###项目编译
+在项目目录下，输入：
+
+	gulp
+
+回车后会自动执行项目编译，相关任务已经在gulpfile.js中进行定义，修改项目目录下的文件时，SCSS和Javascript会自动执行编译、压缩、转义。
+
+###项目提交
+安装完成后，项目路径下会生成.sass-cache和node_modules两个文件夹，前者是SASS编译的缓存文件，后者则包含了安装的插件。
+
+项目提交时，这两个文件夹均不需要提交，忽略即可。
+
+在项目root目录下，找到并修改.ignore文件，增加如下代码：
+
+	.sass-cache
+	node_modules
+
+提交时，.sass-cache和node_modules两个文件夹就会自动忽略。
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass'),
 	autoprefixer = require('gulp-autoprefixer'),
-	minifycss = require('gulp-minify-css'),
-	jshint = require('gulp-jshint'),
-	uglify = require('gulp-uglify'),
-	imagemin = require('gulp-imagemin'),
-	rename = require('gulp-rename'),
-	gif = require('gulp-if'),
+	cache = require('gulp-cache'),
 	clean = require('gulp-clean'),
 	concat = require('gulp-concat'),
-	notify = require('gulp-notify'),
-	cache = require('gulp-cache'),
-	include = require('gulp-include'),
 	connect = require('gulp-connect'),
-	livereload = require('gulp-livereload');
+	cssBase64 = require('gulp-css-base64'),
+	gif = require('gulp-if'),
+	include = require('gulp-include'),
+	jshint = require('gulp-jshint'),
+	livereload = require('gulp-livereload'),
+	minifycss = require('gulp-minify-css'),
+	notify = require('gulp-notify'),
+	rename = require('gulp-rename'),
+	sass = require('gulp-ruby-sass'),
+	uglify = require('gulp-uglify');
 
 gulp.task('connect', function() {
 	connect.server({
@@ -36,8 +36,13 @@ gulp.task('styles', function() {
 	return gulp.src('src/project/**/styles/*.scss')
 		.pipe(sass({
 			style : 'expanded'
-		})).pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		}))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 		.pipe(minifycss())
+		.pipe(gulp.dest('project'))
+		.pipe(cssBase64({
+			baseDir: "../images"
+		}))
 		.pipe(gulp.dest('project'))
 		.pipe(livereload())
 		.pipe(notify({
@@ -57,36 +62,19 @@ gulp.task('scripts', function() {
 		}));
 });
 
-gulp.task('images', function() {
-	return gulp.src('src/project/**/images/*')
-		.pipe(imagemin({
-			optimizationLevel : 3,
-			progressive : true,
-			svgoPlugins : [{
-				removeViewBox : false
-			}]
-		}))
-		.pipe(gulp.dest('project'))
-		.pipe(livereload())
-		.pipe(notify({
-			message : 'images task complete'
-		}));
-});
-
 gulp.task('clean', function() {
-	return gulp.src(['project/**/styles', 'project/**/images', 'project/**/scripts'], {
+	return gulp.src(['project/**/styles/', 'project/**/images/', 'project/**/scripts'], {
 			read : false
 		}).pipe(clean());
 });
 
 gulp.task('default', ['clean'], function() {
-	gulp.start('connect', 'html', 'styles', 'images', 'scripts', 'watch');
+	gulp.start('connect', 'html', 'styles', 'scripts', 'watch');
 });
 
 gulp.task('watch', function() {
 	gulp.watch('src/project/**/*.html', ['html']);
 	gulp.watch('src/project/**/styles/*.scss', ['styles']);
-	gulp.watch('src/project/**/images/*', ['images']);
 	gulp.watch('src/project/**/scripts/*.js', ['scripts']);
 
 	livereload.listen();
